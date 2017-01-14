410e4d03cf742e4b1027e496ec728a47b77a5ea8
mariana-martins <mariana_martins@id.uff.br>
2017-01-14 19:38:23 +1300

    Enemy instances walk away from screen, closes #11

--- js/app.js
+++ js/app.js
@@ -19,8 +19,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x + this.speed * dt;
-    console.log(dt);
+    var newX = this.x + this.speed * dt;
+    if (newX <= 505) {
+        this.x = newX;
+    } else {
+        this.x = -101;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
